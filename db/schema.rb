5b2bccb22e050049e4b7192d1b577c9a9f9303ec
EC2 Default User <ec2-user@ip-172-31-10-151.ec2.internal>
2020-09-11 07:39:48 +0000

    [finish]4-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_095233) do
+ActiveRecord::Schema.define(version: 2020_09_11_070305) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_09_10_095233) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
